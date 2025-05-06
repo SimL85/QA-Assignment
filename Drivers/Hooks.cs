09009cd7dcfb493e9886210cf41a3c09634501a4
SimL85 <lentini.simona@gmail.com>
2025-05-06 08:46:04 +0200

    Integrate Extent Reports for enhanced test reporting
    Updated Hooks.cs to inherit from ExtentReport, adding methods for report initialization, teardown, and scenario handling, including screenshot capture on failure. Introduced dependency injection for IWebDriver using IObjectContainer.
    
    Updated QAAssignment.csproj to include necessary package references for Extent Reports and Reqnroll.
    
    Created ExtentReport.cs to centralize reporting logic, including methods for managing report lifecycle and handling screenshots.

--- Drivers/Hooks.cs
+++ Drivers/Hooks.cs
@@ -1,34 +1,141 @@
+using NUnit.Framework;
+using OpenQA.Selenium.BiDi.Communication;
+using OpenQA.Selenium;
 using Reqnroll;
+using static QAAssignment.Utility.BrowserUtilities;
+using IObjectContainer = Reqnroll.BoDi.IObjectContainer;
+using QAAssignment.Utility;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports;
 
 namespace QAAssignment.Drivers
 {
     [Binding]
-    public sealed class Hooks
+    public sealed class Hooks : ExtentReport
     {
-        // For additional details on Reqnroll hooks see https://go.reqnroll.net/doc-hooks
+        private BrowserInfo browserInfo = new BrowserInfo();
+        IObjectContainer _container;
+        public static string Browser = "chrome";
+        public Hooks(IObjectContainer container)
+        {
+            _container = container;
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            ExtentReportInit();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            Console.WriteLine("Running after test run...");
+            ExtentReportTearDown();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            Console.WriteLine("Running before feature...");
+            _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            Console.WriteLine("Running after feature...");
+        }
 
         [BeforeScenario("@tag1")]
         public void BeforeScenarioWithTag()
         {
-            // Example of filtering hooks using tags. (in this case, this 'before scenario' hook will execute if the feature/scenario contains the tag '@tag1')
-            // See https://go.reqnroll.net/doc-hooks#tag-scoping
 
-            //TODO: implement logic that has to run before executing each scenario
         }
 
         [BeforeScenario(Order = 1)]
-        public void FirstBeforeScenario()
+        public void FirstBeforeScenario(ScenarioContext scenarioContext)
         {
-            // Example of ordering the execution of hooks
-            // See https://go.reqnroll.net/doc-hooks#hook-execution-order
+            Console.WriteLine("Running before scenario...");
+            browserInfo.browser = Browser;
+            IWebDriver driver = GetBrowser(browserInfo, "");
 
-            //TODO: implement logic that has to run before executing each scenario
+
+            _container.RegisterInstanceAs<IWebDriver>(driver);
+
+            _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
         }
 
         [AfterScenario]
         public void AfterScenario()
         {
-            //TODO: implement logic that has to run after executing each scenario
+            Console.WriteLine("Running after scenario...");
+            var driver = _container.Resolve<IWebDriver>();
+
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+
+        [AfterStep]
+        public void AfterStep(ScenarioContext scenarioContext)
+        {
+            Console.WriteLine("Running after step....");
+            string stepType = scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            string stepName = scenarioContext.StepContext.StepInfo.Text;
+
+            var driver = _container.Resolve<IWebDriver>();
+
+            //When scenario passed
+            if (scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName);
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName);
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName);
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName);
+                }
+            }
+
+            //When scenario fails
+            if (scenarioContext.TestError != null)
+            {
+                string screenShotPath = addScreenshot(driver, scenarioContext);
+                string filename = screenShotPath;
+
+                if (stepType == "Given")
+                {
+                    _scenario.CreateNode<Given>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(screenShotPath).Build());
+                }
+                else if (stepType == "When")
+                {
+                    _scenario.CreateNode<When>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(screenShotPath).Build());
+                }
+                else if (stepType == "Then")
+                {
+                    _scenario.CreateNode<Then>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(screenShotPath).Build());
+                }
+                else if (stepType == "And")
+                {
+                    _scenario.CreateNode<And>(stepName).Fail(scenarioContext.TestError.Message,
+                        MediaEntityBuilder.CreateScreenCaptureFromPath(screenShotPath).Build());
+                }
+                TestContext.AddTestAttachment(filename);
+            }
         }
     }
 }
---
+++ Utility/ExtentReport.cs
@@ -0,0 +1,67 @@
+﻿using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using AventStack.ExtentReports.Reporter.Configuration;
+using AventStack.ExtentReports.Reporter.Config;
+
+namespace QAAssignment.Utility
+{
+    public class ExtentReport
+    {
+
+        public static ExtentReports _extentReports;
+        public static ExtentTest _feature;
+        public static ExtentTest _scenario;
+
+        public static String dir = AppDomain.CurrentDomain.BaseDirectory;
+
+        public static String testResPath = "C:\\Screenshots\\VåraSidor\\" + DateTime.Now.ToString("yyyy-MM-dd") + "\\";
+        public static String testResultPath = IsPathExist(testResPath);
+
+        public static void ExtentReportInit()
+        {
+            Console.WriteLine("Inside Extent Report");
+            var htmlReporter = new ExtentSparkReporter(testResultPath);
+            htmlReporter.Config.ReportName = "Automation Status Report";
+            htmlReporter.Config.DocumentTitle = "Automation Status Report";
+            htmlReporter.Config.Theme = Theme.Standard;
+            //htmlReporter.Start();
+
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(htmlReporter);
+            _extentReports.AddSystemInfo("Application", "Youtube");
+            _extentReports.AddSystemInfo("Browser", "Chrome");
+            _extentReports.AddSystemInfo("OS", "Windows");
+        }
+
+        public static void ExtentReportTearDown()
+        {
+            Console.WriteLine("Inside Extent Report  teardown");
+            _extentReports.Flush();
+        }
+
+        public string addScreenshot(IWebDriver driver, ScenarioContext scenarioContext)
+        {
+            ITakesScreenshot takesScreenshot = (ITakesScreenshot)driver;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+            string screenshotLocation = Path.Combine(testResultPath, scenarioContext.ScenarioInfo.Title + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png");
+            screenshot.SaveAsFile(screenshotLocation);
+            return screenshotLocation;
+        }
+
+        public static string IsPathExist(string testResultPath)
+        {
+            if (!Directory.Exists(testResultPath))
+            {
+                Directory.CreateDirectory(testResultPath);
+            }
+            return testResultPath;
+        }
+
+    }
+}
