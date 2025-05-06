df934811a238b0bdcd0b3010d9e4092aa8e6deac
SimL85 <lentini.simona@gmail.com>
2025-05-06 09:15:46 +0200

    Add LoginPage class for web navigation and waits
    Introduces the `LoginPage` class in the `QAAssignment.Pages` namespace.
    Configures explicit and implicit waits for web interactions.
    Includes a method `LoadPage` to navigate to a specified URL with a 2-second delay.

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using static QAAssignment.Utility.BrowserUtilities;
+
+namespace QAAssignment.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver driver = null;
+        private static WebDriverWait wait;
+        private BrowserInfo browserInfo = new BrowserInfo();
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            Console.WriteLine("Page: LoginPage");
+            driver = webDriver;
+            // Configure explicit wait
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+            // Configure implicit wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void LoadPage(string url)
+        {
+            Console.Write("Load url" + url);
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(2000);
+        } 
+    }
+}
